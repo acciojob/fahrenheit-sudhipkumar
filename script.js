2d3861df1ceecf76a73d258b26dc9814d93c1804
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-05-20 10:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function toFahrenheit(celsius) {
- // Write your code here
+  // Convert Celsius to Fahrenheit
+  const fahrenheit = (celsius * 9/5) + 32;
+  
+  // Return the result rounded to two decimal places
+  return fahrenheit.toFixed(2);
 }
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
+
